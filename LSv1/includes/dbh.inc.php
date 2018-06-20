b94e0e11fa5e1ad7fe15118c1ca791260b1b7250
Annan Hutomo Putra <33057938+annanhtmo@users.noreply.github.com>
2018-06-20 16:40:26 +0700

    Add files via upload

---
+++ LSv1/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "db_loginsystem";
+    
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
